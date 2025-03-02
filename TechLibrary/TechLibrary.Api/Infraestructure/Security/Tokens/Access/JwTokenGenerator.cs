e57e23c1b56245b116fd27b851233cdc96c9358e
FernandoZuchi <nandozuchi07@gmail.com>
2025-03-02 18:12:16 -0300

    ✨ feat + 🔒️ lock : access token with JWT

---
+++ TechLibrary/TechLibrary.Api/Infraestructure/Security/Tokens/Access/JwTokenGenerator.cs
@@ -0,0 +1,38 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
+using TechLibrary.Api.Domain.Entities;
+
+namespace TechLibrary.Api.Infraestructure.Security.Tokens.Access;
+
+public class JwTokenGenerator
+{
+    public string Generate(User user)
+    {
+        var claims = new List<Claim>()
+        {
+            new Claim(JwtRegisteredClaimNames.Sub, user.Id.ToString())
+        };
+
+        var tokenDesciptor = new SecurityTokenDescriptor
+        {
+            Expires = DateTime.UtcNow.AddMinutes(60),
+            Subject = new ClaimsIdentity(claims),
+            SigningCredentials = new SigningCredentials(SecurityKey(), SecurityAlgorithms.HmacSha256Signature)
+        };
+
+        var tokenHandler = new JwtSecurityTokenHandler();
+        var securityToken = tokenHandler.CreateToken(tokenDesciptor);
+        return tokenHandler.WriteToken(securityToken);
+    }
+
+    private static SymmetricSecurityKey SecurityKey()
+    {
+        var signInkey = "nkJIlrTB0ryLRJuTFc6C7z39OqAuC7Cx";
+
+        var symmetricKey = Encoding.UTF8.GetBytes(signInkey);
+
+        return new SymmetricSecurityKey(symmetricKey);
+    }
+}
--- TechLibrary/TechLibrary.Api/UseCases/Users/Register/RegisterUserUseCase.cs
+++ TechLibrary/TechLibrary.Api/UseCases/Users/Register/RegisterUserUseCase.cs
@@ -1,6 +1,8 @@
-﻿using TechLibrary.Api.Domain.Entities;
+﻿using FluentValidation.Results;
+using TechLibrary.Api.Domain.Entities;
 using TechLibrary.Api.Infraestructure.DataAccess;
 using TechLibrary.Api.Infraestructure.Security.Cryptography;
+using TechLibrary.Api.Infraestructure.Security.Tokens.Access;
 using TechLibrary.Communication.Requests;
 using TechLibrary.Communication.Responses;
 using TechLibrary.Exception;
@@ -11,7 +13,8 @@ public class RegisterUserUseCase
 {
     public ResponseRegisteredUserJson Execute(RequestUserJson request)
     {
-        Validate(request);
+        var dbContext = new TechLibraryDbContext();
+        Validate(request, dbContext);
 
         var cryptography = new BCryptAlgorithm();
 
@@ -22,25 +25,33 @@ public class RegisterUserUseCase
             Password = cryptography.HashPassword(request.Password),
         };
 
-        var dbContext = new TechLibraryDbContext();
-
         // query utilizando entity framework como ORM
         dbContext.Users.Add(entity);
         dbContext.SaveChanges();
 
+        var tokenGenerator = new JwTokenGenerator();
+
         return new ResponseRegisteredUserJson
-        { 
+        {
             Name = entity.Name,
+            AccessToken = tokenGenerator.Generate(entity)
         };
     }
 
-    private void Validate(RequestUserJson request)
+    private void Validate(RequestUserJson request, TechLibraryDbContext dbContext)
     {
         var validator = new RegisterUserValidator();
 
         var result = validator.Validate(request);
 
-        if(result.IsValid == false)
+        var existUserWithEmail = dbContext.Users.Any(user => user.Email.Equals(request.Email));
+
+        if (existUserWithEmail)
+        {
+            result.Errors.Add(new ValidationFailure("Email", "E-mail já registrado na plataforma."));
+        }
+
+        if (result.IsValid == false)
         {
             var errorMessages = result.Errors.Select(error => error.ErrorMessage).ToList();
 
